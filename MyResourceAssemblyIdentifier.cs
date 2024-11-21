c386534fd0dcbd225babd709c2bc10648f96aafd
Jonne Kleijer <jonne@nautilusit.nl>
2024-11-21 09:46:01 +0100

    feat: initial commit

---
+++ MyResourceAssemblyIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+
+namespace MyExtension;
+
+[Export(typeof(IResourceAssemblyIdentifier))]
+[Name(nameof(MyResourceAssemblyIdentifier))]
+internal sealed class MyResourceAssemblyIdentifier : IResourceAssemblyIdentifier
+{
+    public ValueTask<FontDefinition[]> GetFontDefinitionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
